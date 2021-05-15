859447934ec7e7ea0f25a0d5d75a43af76405f1d
ashouy <joaopaulokad@gmail.com>
2021-05-15 16:29:13 -0300

    remove/add itens in cart and add bump effect with useEffect

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -2,15 +2,31 @@ import React, { useContext } from "react";
 import Modal from "../UI/modal/Modal";
 import classes from "./Cart.module.css";
 import CartContext from "../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
   const ctx = useContext(CartContext);
   const totalAmount = `$${ctx.totalAmount.toFixed(2)}`;
-  const hasItems = ctx.items.length > 0
+  const hasItems = ctx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+      ctx.removeItem(id)
+  };
+  const cartItemAddHandler = (item) => {
+      ctx.addItem(item)
+  };
+
   const cartItens = (
-    <ul className={classes["cart-itens"]}>
+    <ul className={classes["cart-items"]}>
       {ctx.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
--- src/components/layout/HeaderCartButton.js
+++ src/components/layout/HeaderCartButton.js
@@ -1,14 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "../cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../store/cart-context";
 const HeaderCartButton = (props) => {
   const ctx = useContext(CartContext);
+  const { items } = ctx;
+  const [btnBump, setBtnBump] = useState(false);
   const countItens = ctx.items.reduce((current, item) => {
     return current + item.amount;
   }, 0);
+
+  const btnClasses = `${classes.button} ${btnBump ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnBump(true);
+
+    const timer = setTimeout(() => {
+      setBtnBump(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
